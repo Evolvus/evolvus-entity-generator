c26a52d8b04296b8016b4379351e90ffbd38a974
ennovatenow <anish.thomas@evolvussolutions.com>
2018-05-25 09:00:32 +0530

    struture in place, need few refinements

--- app.js
+++ app.js
@@ -7,22 +7,28 @@
  */
 const _ = require("underscore");
 const argv = require('minimist')(process.argv.slice(2));
-
+const fs = require("fs");
+const gen = require("./generator")
+/*
+ ** Minimum command line requirement for this CLI application is entity
+ ** If that is not provided exit the application.
+ */
 if (!_.has(argv, "entity")) {
   console.log(`Usage: ${process.argv[1]} --entity <entity name> `);
   console.log(`e.g. ${process.argv[1]} --entity application`);
   process.exit(-1);
 }
 
-const entityObject = _.pick(argv, "entity");
-
-console.log("entity is: " + entity.entity);
-
 /*
- ** Default constants
+ ** Get the entity, now that we are sure it exists in argv
  */
+const entity = _.pick(argv, "entity")
+  .entity;
+
 const orgName = "evolvus";
+/*
+ ** Derive the different values needed for replacement
+ */
+gen.initialize(orgName, entity);
 
-console.dir(argv);
-
-console.log("got here");
+console.log("done");
---
+++ generator.js
@@ -0,0 +1,96 @@
+'use strict';
+
+const fs = require("fs");
+const moustache = require("moustache");
+
+
+function initialize(orgName, entity) {
+  const context = initContext(orgName, entity);
+  createDirStructure(context);
+  addFiles(context);
+};
+
+function initContext(orgName, entity) {
+  const context = {
+    "moduleName": `${orgName}-${entity}`
+  };
+  return context;
+};
+
+/*
+ ** Create the folder structure for entity projects if it does not exist.
+ ** The default folder structure is as follows,
+ ** entity-module
+ ** entity-module/db
+ ** entity-module/model
+ ** entity-module/test
+ */
+function createDirStructure(context) {
+  /*
+   ** List of the folders to be createdt
+   */
+  var folders = [
+    context.moduleName,
+    context.moduleName + "/db",
+    context.moduleName + "/model",
+    context.moduleName + "/test"
+  ];
+
+  /*
+   ** For each folder in list create if not already exists
+   */
+  folders.forEach((folder) => {
+    if (!fs.existsSync(folder)) {
+      fs.mkdirSync(folder);
+    }
+  });
+};
+
+/*
+ ** Add the template code for the project. Moustache templates from the
+ ** templates folder is read, processed and written to the approprite
+ ** directories.
+ */
+function addFiles(context) {
+  var baseFolder = context.moduleName;
+  var templates = listTemplates(__dirname + "/templates");
+
+  templates.forEach((fileName) => {
+    var templateText = fs.readFileSync(fileName)
+      .toString();
+    var text = moustache.render(templateText, context);
+    fs.writeFileSync(fileName.replace(__dirname + "/templates", baseFolder), text, {
+      "flag": "wx"
+    });
+  });
+};
+
+/*
+ ** List all the templates in the different folders
+ */
+function listTemplates() {
+  var folders = [
+    __dirname + "/templates",
+    __dirname + "/templates/db",
+    __dirname + "/templates/model",
+    __dirname + "/templates/test"
+  ];
+
+  var templates = [];
+  folders.forEach((folder) => {
+    var fileList = fs.readdirSync(folder);
+    fileList.forEach((file) => {
+      var template = folder + "/" + file;
+      if (!fs.statSync(template)
+        .isDirectory()) {
+        templates.push(template);
+      }
+    });
+  });
+  return templates;
+};
+
+
+module.exports = {
+  "initialize": initialize
+};
