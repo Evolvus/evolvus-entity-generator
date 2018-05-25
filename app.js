f01cbabc39de1ed964623fce3629c0467a3e51fb
ennovatenow <anish.thomas@evolvussolutions.com>
2018-05-25 06:11:20 +0530

    added bin property to package.json to make this a cli application

--- app.js
+++ app.js
@@ -5,10 +5,24 @@
 /*
  ** Get all the required packages
  */
+const _ = require("underscore");
+const argv = require('minimist')(process.argv.slice(2));
 
-const orgName = 'evolvus';
+if (!_.has(argv, "entity")) {
+  console.log(`Usage: ${process.argv[1]} --entity <entity name> `);
+  console.log(`e.g. ${process.argv[1]} --entity application`);
+  process.exit(-1);
+}
 
-var argv = require('minimist')(process.argv.slice(2));
+const entityObject = _.pick(argv, "entity");
+
+console.log("entity is: " + entity.entity);
+
+/*
+ ** Default constants
+ */
+const orgName = "evolvus";
+
 console.dir(argv);
 
-console.log('got here');
+console.log("got here");
